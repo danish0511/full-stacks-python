e81743de3104eefba0ba02308501d59fb559c754
danish0511 <dsdd0511@gmail.com>
2024-07-15 23:22:20 +0530

    routing

--- full_stack_python/full_stack_python.py
+++ full_stack_python/full_stack_python.py
@@ -5,6 +5,7 @@ import reflex as rx
 from rxconfig import config
 
 from .ui.base import base_page
+from . import pages, navigation
 
 class State(rx.State):
     """The app state."""
@@ -18,30 +19,24 @@ class State(rx.State):
     
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return base_page(
-        rx.vstack(
+    my_child=rx.vstack(
             rx.heading(State.label, size="9"),
             rx.text(
                 "Get started by editing ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
                 size="5",
             ),
-            rx.input(
-                default_value=State.label,
-                on_click=State.did_change,
-                on_change=State.handle_title_input_change
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
             spacing="5",
             justify="center",
+            align="center",
             min_height="85vh",
-        ),
-    )
+            id="my-child"
+        )
+    return base_page(my_child)
 
 
 app = rx.App()
 app.add_page(index)
+app.add_page(pages.about_page, route=navigation.routes.ABOUT_US_ROUTE)
+app.add_page(pages.pricing_page, route=navigation.routes.PRICING_ROUTE)
+app.add_page(pages.contact_page, route=navigation.routes.CONTACT_US_ROUTE)
---
+++ full_stack_python/navigation/routes.py
@@ -0,0 +1,6 @@
+HOME_ROUTE="/"
+ABOUT_US_ROUTE="/about"
+CONTACT_US_ROUTE="/contact"
+PRICING_ROUTE="/pricing"
+
+
