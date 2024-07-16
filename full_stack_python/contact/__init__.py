0b956ba55e09f1f140695acf5d471dfaa89381b3
danish0511 <dsdd0511@gmail.com>
2024-07-16 23:44:14 +0530

    decouple contact

---
+++ full_stack_python/contact/__init__.py
@@ -0,0 +1,9 @@
+from .state import ContactState
+from .form import contact_form
+from .page import contact_page
+
+__all__ = [
+    'contact_form',
+    'ContactState',
+    'contact_page',
+]
---
+++ full_stack_python/contact/model.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timezone
+import asyncio
+import reflex as rx
+from sqlmodel import Field
+import sqlalchemy
+
+def get_utc_now() -> datetime:
+    return datetime.now(timezone.utc)
+
+class ContactEntryModel(rx.Model, table=True):
+    user_id: int | None = None
+    first_name: str
+    last_name: str = Field(nullable=True)
+    email: str = Field(nullable=True)
+    message: str
+    created_at: datetime = Field(
+        default_factory=get_utc_now,
+        sa_type=sqlalchemy.DateTime(timezone=True),
+        sa_column_kwargs={
+            'server_default': sqlalchemy.func.now()
+        },
+        nullable=False
+    )
