4fdcefab08eae4ac4d69b586850ec2b1869b3366
danish0511 <dsdd0511@gmail.com>
2024-07-18 23:29:21 +0530

    redirect url

--- full_stack_python/blog/detail.py
+++ full_stack_python/blog/detail.py
@@ -3,9 +3,16 @@ from ..ui.base import base_page
 from . import state
 
 def blog_post_detail_page() -> rx.Component:
+    can_edit = True
+    edit_link = rx.link("Edit", href=f"{state.BlogPostState.blog_post_edit_url}")
+    edit_link_el = rx.cond(can_edit, edit_link, rx.fragment(''))
     my_child=rx.vstack(
-            rx.heading(state.BlogPostState.post.title, size="9"),
-            rx.text(state.BlogPostState.post.content),
+            rx.hstack(
+                rx.heading(state.BlogPostState.post.title, size="9"),
+                edit_link_el,
+                align='end'
+            ),
+            rx.text(state.BlogPostState.post.content, white_space='pre-wrap'),
             spacing="5",
             align="center",
             min_height="85vh"
--- full_stack_python/blog/state.py
+++ full_stack_python/blog/state.py
@@ -3,6 +3,11 @@ from typing import Optional, List
 from sqlmodel import select
 
 from .model import BlogPostModel
+from .. import navigation
+
+BLOG_POSTS_ROUTE=navigation.routes.BLOG_POSTS_ROUTE
+if BLOG_POSTS_ROUTE.endswith("/"):
+    BLOG_POSTS_ROUTE = BLOG_POSTS_ROUTE[:-1]
 
 class BlogPostState(rx.State):
     posts: List['BlogPostModel'] = []
@@ -12,6 +17,19 @@ class BlogPostState(rx.State):
     @rx.var
     def blog_post_id(self):
         return self.router.page.params.get("blog_id", "")
+    
+    @rx.var
+    def blog_post_url(self):
+        if not self.post:
+            return f"{BLOG_POSTS_ROUTE}"
+        return f"{BLOG_POSTS_ROUTE}/{self.post.id}"
+    
+    @rx.var
+    def blog_post_edit_url(self):
+        if not self.post:
+            return f"{BLOG_POSTS_ROUTE}"
+        return f"{BLOG_POSTS_ROUTE}/{self.post.id}/edit"
+    
     
     def get_post_detail(self):
         with rx.session() as session:
@@ -24,6 +42,9 @@ class BlogPostState(rx.State):
                 )
             ).one_or_none()
             self.post = result
+            if result is None:
+                self.post_content=""
+                return
             self.post_content = result.content
     
     def load_posts(self):
@@ -57,6 +78,14 @@ class BlogPostState(rx.State):
             session.add(post)
             session.commit()
             session.refresh(post)
+            self.post = post
+    
+    def to_blog_post(self, edit_page=False):
+        if not self.post:
+            return rx.redirect(BLOG_POSTS_ROUTE)
+        if edit_page:
+            return rx.redirect(f"{self.blog_post_edit_url}")
+        return rx.redirect(f"{self.blog_post_url}")
 
 
 class BlogAddPostFormState(BlogPostState):
@@ -65,6 +94,7 @@ class BlogAddPostFormState(BlogPostState):
     def handle_submit(self, form_data):
         self.form_data = form_data
         self.add_post(form_data)
+        return self.to_blog_post(edit_page=True)
         
 class BlogEditFormState(BlogPostState):
     form_data: dict = {}
@@ -75,4 +105,6 @@ class BlogEditFormState(BlogPostState):
         post_id = form_data.pop('post_id')
         updated_data = {**form_data}
         self.save_post_edits(post_id, updated_data)
+        return self.to_blog_post()
+        
         
