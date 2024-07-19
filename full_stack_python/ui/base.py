b29e37921d1bbd3d53ec883c04dc422598145f6f
danish0511 <dsdd0511@gmail.com>
2024-07-19 23:45:48 +0530

    sidebar customize

--- full_stack_python/ui/base.py
+++ full_stack_python/ui/base.py
@@ -1,28 +1,30 @@
 import reflex as rx
 from .nav import navbar
+from .dashboard import base_dashboard_page
 
-def base_page(child: rx.Component, hide_navbar=False, *args, **kwargs) -> rx.Component:
-    
+def base_layout_component(child, *args, **kwargs) -> rx.Component:
+    return rx.fragment(
+            navbar(),
+            rx.box(
+                child,
+                # bg=rx.color("accent", 3),
+                padding="1em",
+                width="100%",
+                id="my-content-area-el"
+            ),
+            rx.logo(),
+            rx.color_mode.button(position="bottom-left"),
+            padding='10em',
+            id="my-base-container"
+    )
+
+def base_page(child: rx.Component, *args, **kwargs) -> rx.Component:
+    is_logged_in = True
     if not isinstance(child, rx.Component):
         child=rx.heading("This is not a valid child element")
-    
-    if hide_navbar:
-        return rx.container(
-        child,
-        rx.logo(),
-        rx.color_mode.button(position="bottom-left"),
-    )
-        
-    return rx.fragment(
-        navbar(),
-        rx.box(
-            child,
-            # bg=rx.color("accent", 3),
-            padding="1em",
-            width="100%",
-            id="my-content-area-el"
-        ),
-        rx.logo(),
-        rx.color_mode.button(position="bottom-left"),
-        id="my-base-container"
+
+    return rx.cond(
+        is_logged_in,
+        base_dashboard_page(child, *args, **kwargs),
+        base_layout_component(child, *args, **kwargs)
     )
---
+++ full_stack_python/ui/dashboard.py
@@ -0,0 +1,24 @@
+import reflex as rx
+from .sidebar import sidebar
+
+def base_dashboard_page(child: rx.Component, *args, **kwargs) -> rx.Component:
+    
+    if not isinstance(child, rx.Component):
+        child=rx.heading("This is not a valid child element")
+        
+    return rx.fragment(
+        rx.hstack(
+            sidebar(),
+            rx.box(
+                child,
+                rx.logo(),
+                # bg=rx.color("accent", 3),
+                padding="1em",
+                width="100%",
+                id="my-content-area-el"
+            ),
+        ),
+        # rx.color_mode.button(position="bottom-left"),
+        padding='10em',
+        id="my-base-container"
+    )
